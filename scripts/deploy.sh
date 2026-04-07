7ec3ff738448f781a19fbce337404d2fefa64294
Vivek Agarwal <62823320+singhalvivek@users.noreply.github.com>
2026-04-07 16:56:42 +0530

    Revert "Kill flask 5000 and restart (#57)"
    This reverts commit 0cfedac2628aa3c9672fa2ea441c48d64f0be2dc.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -26,10 +26,7 @@ echo "Generating .env file using Python script..."
 python3 get_env.py
 
 echo "Restarting service..."
-PID=$(lsof -t -i:5000) || true
-if [ ! -z "$PID" ]; then
-	kill -9 $PID
-fi
+pkill -f flask || true
 cd /opt/ai-agent-boilerplate/code
 export FLASK_APP=app.py
 nohup flask run --host=0.0.0.0 --port=5000 > flask.log 2>&1 &
